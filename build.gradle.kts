b2b8578b39682d869688242dbf775321f3733762
Tom Hermann <tom.hermann@wwt.com>
2021-03-29 15:37:21 -0500

    Specify gradle toolchain language version, to grab Java 16 automatically. Upgrade to latest gradle release candidate for Java 16 support.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.31"
     application
     id("org.openjfx.javafxplugin") version "0.0.9"
-    id("org.beryx.jlink") version "2.22.1"
+    id("org.beryx.jlink") version "2.23.5"
 }
 
 val compileKotlin: KotlinCompile by tasks
 val compileJava: JavaCompile by tasks
 compileJava.destinationDir = compileKotlin.destinationDir
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
+}
 
 application {
     mainModule.set("test.kotlin")
